d9c7c958b7c96cc2d33de50918b86fa377ffb0e2
Mohamed Elbahy <vb.supporter@gmail.com>
2024-03-02 21:28:22 +0200

    historical period view part 2

--- lib/features/home/presentation/views/historical_period_view.dart
+++ lib/features/home/presentation/views/historical_period_view.dart
@@ -1,6 +1,9 @@
+import 'package:dalel/core/utils/app_strings.dart';
 import 'package:dalel/core/utils/app_styles.dart';
+import 'package:dalel/core/widgets/custom_data_list_view.dart';
 import 'package:dalel/core/widgets/custom_text_header.dart';
 import 'package:dalel/features/home/data/models/historical_periods_model.dart';
+import 'package:dalel/features/home/presentation/widgets/custom_category_list_view.dart';
 import 'package:dalel/features/home/presentation/widgets/home_sections/home_app_bar_section.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -29,19 +32,41 @@ class HistoricalPeriodView extends StatelessWidget {
             )),
             const SliverToBoxAdapter(child: SizedBox(height: 47)),
             SliverToBoxAdapter(
-                child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SizedBox(
-                    height: 220,
-                    width: 196,
-                    child: Text(model.description,
-                        style: AppStyles.poppins500Style14
-                            .copyWith(color: Colors.black, height: 1.5))),
-                const SizedBox(width: 16),
-                Image.network(model.imageUrl, width: 131, height: 203)
-              ],
-            ))
+                child: Stack(clipBehavior: Clip.none, children: [
+              Positioned(
+                  top: -30,
+                  left: -15,
+                  child: SvgPicture.network(model.bgUrl,
+                      width: 145, height: 145, color: const Color(0xffE8DAD6))),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                      height: 220,
+                      width: 215,
+                      child: Text(model.description,
+                          style: AppStyles.poppins500Style14
+                              .copyWith(color: Colors.black, height: 1.5))),
+                  const SizedBox(width: 16),
+                  Image.network(model.imageUrl, width: 131, height: 203)
+                ],
+              ),
+            ])),
+            const SliverToBoxAdapter(child: SizedBox(height: 22)),
+            SliverToBoxAdapter(
+                child:
+                    CustomTextHeader(text: '${model.name} ${AppStrings.wars}')),
+            const SliverToBoxAdapter(child: SizedBox(height: 16)),
+            SliverToBoxAdapter(
+                child: CustomDataListView(
+                    dataList: model.wars, routePath: '/WarsView')),
+            const SliverToBoxAdapter(child: SizedBox(height: 24)),
+            const SliverToBoxAdapter(
+                child: CustomTextHeader(text: AppStrings.recommendations)),
+            const SliverToBoxAdapter(child: SizedBox(height: 16)),
+            const SliverToBoxAdapter(
+              child: CustomCategoryListView(),
+            )
           ],
         ),
       ),
